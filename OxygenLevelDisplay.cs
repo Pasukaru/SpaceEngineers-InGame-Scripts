ca9dd70f0dc2aa28f279eb9fde9114343e32aa56
Pascal Ludwig <pasukaru@live.de>
2015-04-23 18:59:09 +0200

    Removed support for real-time updates.
    It is hardly useful as one second delay between updates should be fast
    enough.
    This also simplified the script.

--- OxygenLevelDisplay.cs
+++ OxygenLevelDisplay.cs
@@ -7,59 +7,48 @@ public class OxygenLevelDisplay
 {
     IMyGridTerminalSystem GridTerminalSystem = null;
     #region CodeEditor
-    //************************ Oxygen Level Display ************************\\        
-    // Build a Timer Block and configure it so that the first action points to this Programmable Block -> "Run".   
-    // Enter the names of the blocks in the variables below.   
-    // Make sure all blocks belong to the same user, hit the Run button and that's it!  
+    //************************ Oxygen Level Display ************************\\
+    // Build an LCD Panel (I recommend a wide one with font size 4.5).
+    // Build a Timer Block and configure it:
+    // Action 1: points to this Programmable Block -> "Run".
+    // Action 2: points to the Timer Block (itself) -> "Start".
+    // Set the delay to 1 second for faster updates. Higher values will consume
+    // less CPU though.
+    // Make sure all blocks belong to the same user.
+    // Configure this script (see below).
+    // Start the timer.
 
     //***** Configuration *****\\   
 
     //Name of the LCD/Text Panel that will display the text  
     const string LCD_PANEL = "LCD (Oxygen Status)";
 
-    //Timerblock  
-    const string TIMER_BLOCK = "Timer Block (Oxygen Status)";
-
-    //Update every second (1000ms).  
-    //Higher values result in less frequent updates to the panel but will also consume less CPU.  
-    const long INTERVAL = 1000;
-
     //Message format 
     const string FORMAT = "Oxygen Level\nAvg: {0:0.00000}%\nTanks: {1:00}\nTime Left: {2:00}:{3:00}:{4:00}";
 
+    //Set this to false if you don't know why something is not working properly.
+    //However, you will have to recompile (Remember & Exit) the script if an error occurs.
+    const bool IGNORE_ERRORS = true;
+
     //***** DO NOT EDIT BELOW THIS LINE *****//  
-
-    long last = 0;
-
-    void nextTick()
-    {
-        var tb = GridTerminalSystem.GetBlockWithName(TIMER_BLOCK);
-        if (tb != null && (tb is IMyTimerBlock))
-        {
-            tb.GetActionWithName("TriggerNow").Apply(tb);
-        }
-    }
 
     void Main()
     {
-        var now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-        if (now - last < INTERVAL)
+
+        var panel_block = GridTerminalSystem.GetBlockWithName(LCD_PANEL) as IMyTerminalBlock;
+
+        if (panel_block == null || !(panel_block is IMyTextPanel))
         {
-            nextTick();
+            if (!IGNORE_ERRORS)
+            {
+                throw new Exception("Failed to find panel with name: " + LCD_PANEL);
+            }
             return;
         }
-        last = now;
 
-        var panel = GridTerminalSystem.GetBlockWithName(LCD_PANEL) as IMyTerminalBlock;
-
-        if (panel == null || !(panel is IMyTextPanel))
-        {
-            nextTick();
-            return;
-        }
+        IMyTextPanel panel = (IMyTextPanel)panel_block;
 
         double oxygen = 0;
-
         var tanks = new List<IMyTerminalBlock>();
         GridTerminalSystem.GetBlocksOfType<IMyOxygenTank>(tanks);
         if (tanks.Count > 0)
@@ -72,10 +61,8 @@ public class OxygenLevelDisplay
         }
 
         var timeLeft = TimeSpan.FromSeconds(Math.Min(oxygen * tanks.Count / 0.0001, 359999));
-
-        (panel as IMyTextPanel).WritePublicText(String.Format(FORMAT, oxygen, tanks.Count, Math.Floor(timeLeft.TotalHours), timeLeft.Minutes, timeLeft.Seconds));
-        (panel as IMyTextPanel).ShowPublicTextOnScreen();
-        nextTick();
+        panel.WritePublicText(String.Format(FORMAT, oxygen, tanks.Count, Math.Floor(timeLeft.TotalHours), timeLeft.Minutes, timeLeft.Seconds));
+        panel.ShowPublicTextOnScreen();
     }
     #endregion
 }
