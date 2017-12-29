f9989070810d3e95dda91c07f09d43d95e9b774f
Pascal Ludwig <pascal.ludwig@mantro.net>
2017-12-29 05:18:23 +0100

    Fixed including hydrogen tanks
    Also added an option to calculate remaining time for more than one person

--- OxygenLevelDisplay.cs
+++ OxygenLevelDisplay.cs
@@ -7,70 +7,80 @@ public class OxygenLevelDisplay
 {
     IMyGridTerminalSystem GridTerminalSystem = null;
     #region CodeEditor
-    //************************ Oxygen Level Display ************************\\
-    // Build an LCD Panel (I recommend a wide one with font size 4.5).
-    // Build a Timer Block and configure it:
-    // Action 1: points to this Programmable Block -> "Run".
-    // Action 2: points to the Timer Block (itself) -> "Start".
-    // Set the delay to 1 second for faster updates. Higher values will consume
-    // less CPU though.
-    // Make sure all blocks belong to the same user.
-    // Configure this script (see below).
-    // Start the timer.
+    //************************ Oxygen Level Display ************************\\ 
+    // Build an LCD Panel (I recommend a wide one with font size 4.5). 
+    // Build a Timer Block and configure it: 
+    // Action 1: points to this Programmable Block -> "Run". 
+    // Action 2: points to the Timer Block (itself) -> "Start". 
+    // Set the delay to 1 second for faster updates. Higher values will consume 
+    // less CPU though. 
+    // Make sure all blocks belong to the same user. 
+    // Configure this script (see below). 
+    // Start the timer. 
+     
+    //***** Configuration *****\\    
+     
+    //Name of the LCD/Text Panel that will display the text   
+    const string LCD_PANEL = "LCD (Oxygen Status)"; 
+     
+    //Set this to false if you want to ignore docked ships 
+    const bool IGNORE_OTHER_GRIDS = true; 
 
-    //***** Configuration *****\\   
+    // Number of astronauts is used to calculate the remaining time. i.e: 2 would have have the time left.
+    const int ASTRONAUTS = 1;
+     
+    //Message format  
+    const string FORMAT = "Oxygen Level: {0:0.00}%\nCapacity: {5:00000}/{6:00000}\nTanks: {1}\nTime Left: {2:00}:{3:00}:{4:00}"; 
+     
+    //Set this to false if you don't know why something is not working properly. 
+    //However, you will have to recompile (Remember & Exit) the script if an error occurs. 
+    const bool IGNORE_ERRORS = true; 
+     
+    //***** DO NOT EDIT BELOW THIS LINE *****//   
 
-    //Name of the LCD/Text Panel that will display the text  
-    const string LCD_PANEL = "LCD (Oxygen Status)";
-	
-    //Set this to false if you want to ignore docked ships
-    const bool IGNORE_OTHER_GRIDS = true;
-
-    //Message format 
-    const string FORMAT = "Oxygen Level\nAvg: {0:0.00000}%\nTanks: {1:00}\nTime Left: {2:00}:{3:00}:{4:00}";
-
-    //Set this to false if you don't know why something is not working properly.
-    //However, you will have to recompile (Remember & Exit) the script if an error occurs.
-    const bool IGNORE_ERRORS = true;
-
-    //***** DO NOT EDIT BELOW THIS LINE *****//  
-
-    void Main()
-    {
-
-        var panel_block = GridTerminalSystem.GetBlockWithName(LCD_PANEL) as IMyTerminalBlock;
-
-        if (panel_block == null || !(panel_block is IMyTextPanel))
-        {
-            if (!IGNORE_ERRORS)
-            {
-                throw new Exception("Failed to find panel with name: " + LCD_PANEL);
+    const double O2_CONSUMPTION = 0.063;
+     
+    void Main() 
+    { 
+     
+        var panel_block = GridTerminalSystem.GetBlockWithName(LCD_PANEL) as IMyTerminalBlock; 
+     
+        if (panel_block == null || !(panel_block is IMyTextPanel)) 
+        { 
+            if (!IGNORE_ERRORS) 
+            { 
+                throw new Exception("Failed to find panel with name: " + LCD_PANEL); 
+            } 
+            return; 
+        } 
+     
+        IMyTextPanel panel = (IMyTextPanel)panel_block; 
+     
+        var tanks = new List<IMyTerminalBlock>(); 
+        GridTerminalSystem.GetBlocksOfType<IMyGasTank>(tanks, x=>x.DetailedInfo.Split(' ')[1] == "Oxygen"); 
+        if (IGNORE_OTHER_GRIDS) 
+        { 
+            tanks = tanks.Where(tank => tank.CubeGrid == Me.CubeGrid).ToList(); 
+        } 
+     
+        var oxygen = 0f;
+        var capacity = 0f;
+        if (tanks.Count > 0) 
+        { 
+            for (int i = 0; i < tanks.Count; i++) 
+            { 
+                var tank = (tanks[i] as IMyGasTank);
+                capacity += tank.Capacity;
+                oxygen += tank.Capacity * tank.FilledRatio;
             }
-            return;
         }
+        var percent = oxygen / capacity * 100;
 
-        IMyTextPanel panel = (IMyTextPanel)panel_block;
-
-        double oxygen = 0;
-        var tanks = new List<IMyTerminalBlock>();
-        GridTerminalSystem.GetBlocksOfType<IMyGasTank>(tanks);
-		if (IGNORE_OTHER_GRIDS)
-		{
-		    tanks = tanks.Where(tank => tank.CubeGrid == Me.CubeGrid).ToList();
-		}
-		
-        if (tanks.Count > 0)
-        {
-            for (int i = 0; i < tanks.Count; i++)
-            {
-                oxygen += (tanks[i] as IMyGasTank).FilledRatio;
-            }
-            oxygen /= tanks.Count / 100f;
-        }
-
-        var timeLeft = TimeSpan.FromSeconds(Math.Min(oxygen * tanks.Count / 0.0001, 359999));
-        panel.WritePublicText(String.Format(FORMAT, oxygen, tanks.Count, Math.Floor(timeLeft.TotalHours), timeLeft.Minutes, timeLeft.Seconds));
-        panel.ShowPublicTextOnScreen();
+        var timeLeft = TimeSpan.FromSeconds(Math.Min(oxygen / O2_CONSUMPTION / ASTRONAUTS, 359999)); 
+        panel.WritePublicText(String.Format(FORMAT, percent, tanks.Count, 
+          Math.Floor(timeLeft.TotalHours), timeLeft.Minutes, timeLeft.Seconds, 
+          oxygen, capacity)
+        );
     }
     #endregion
 }
